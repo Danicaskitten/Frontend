f503ee3ca7d4803dae8e6f80c4adf0dec17d6fcf
Danica <danica.pivalica@gmail.com>
2016-11-26 22:04:35 +0100

    Chat connected to bot server

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { mountToDom } from 'corky/tags/mount';
 import { Route } from 'corky/routing/route'
 import { AppContainer} from './element/appContainer';
 import { appReducer, goToView, PageActive } from './ducks/appDuck';
-import {chatReducer} from './ducks/chatDuck';
+import {chatReducer,startConversation,getChatMessages} from './ducks/chatDuck';
 import IModel from './model';
+var timingInterval = undefined;
 
 export var app = new App<IModel>(
     {
@@ -17,32 +18,51 @@ app.setRouter([
         address: "/dashboard",
         on: () => {
             app.dispatch(goToView.payload({ view: PageActive.Dashboard }));
+            if(timingInterval)
+                clearInterval(timingInterval);
+            timingInterval = undefined;
         }
     }),
     new Route({
         address: "/profile",
         on: () => {
             app.dispatch(goToView.payload({ view: PageActive.Profile }));
+            if(timingInterval)
+                clearInterval(timingInterval);
+            timingInterval = undefined;
         }
     }),
     new Route({
         address: "/search",
         on: () => {
             app.dispatch(goToView.payload({ view: PageActive.Search }));
+            if(timingInterval)
+                clearInterval(timingInterval);
+            timingInterval = undefined; 
+            
         },
         subroutes: [
             new Route({
                 address: "/:query",
                 on: (q) => {
+                    if(timingInterval)
+                        clearInterval(timingInterval);
+                    timingInterval = undefined;
                     // app.dispatch(giphySearch.payload({query:{q: q}}));
                 }
-            })
+            }) 
         ]
     }),
     new Route({
         address: "/chat",
         on: () => {
+            app.dispatch(startConversation.payload({}));
             app.dispatch(goToView.payload({ view: PageActive.Chat }));
+            if(timingInterval === undefined){
+               timingInterval = setInterval(function(k){
+                    app.dispatch(getChatMessages);
+                },1000)
+            }
         }
     })
 ], '/dashboard');
--- src/service/chatService.ts
+++ src/service/chatService.ts
@@ -1,6 +1,6 @@
 import { Service } from 'corky/flux/service';
 import {IModel} from '../model';
-import {sendBotReply, pushMessage} from '../ducks/chatDuck';
+import {sendBotReply, pushMessage,ISendMessageRequestPayload} from '../ducks/chatDuck';
 
 export class ChatService extends Service {
     selector = (state: IModel) => ({
@@ -9,13 +9,15 @@ export class ChatService extends Service {
                 url: reply.url,
                 name: reply.username,
                 text: reply.replyText,
-                time: reply.timestamp.getHours() + ' ' + reply.timestamp.getMinutes(),
+                time: reply.timestamp.getHours() + ':' +  (reply.timestamp.getMinutes()<10?'0':'') + reply.timestamp.getMinutes(),
                 me: reply.sender === 1
             }
         })
     });
     actions = {
-        sendReply: () => sendBotReply.payload({ query: { text: "" } }),
+        sendReply: () => sendBotReply.payload({
+            data: undefined, options: undefined, template: undefined
+        }),
         pushReply: (text) => pushMessage.payload({ text: text })
     }
 }
