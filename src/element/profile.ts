4c409e55e4d54db744bed4054a5f00ff9a0e0b79
Danica <danica.pivalica@gmail.com>
2017-01-16 20:11:31 +0100

    MOV-204 #time 3h

--- src/element/profile.ts
+++ src/element/profile.ts
@@ -6,7 +6,9 @@ import profileService from '../service/profileService';
 @template("profile", profileService)
 export abstract class Profile extends Element {
         username: string;
-        abstract sendGenres (genres: Array<number>)
+        abstract sendGenres (genres: Array<number>);
+        abstract changePass(old: string, newP: string,confirm: string);
+        genresOpen: boolean = true;
 
         saveChanges(){ 
                 var arrayOfTrue = [];
@@ -16,5 +18,17 @@ export abstract class Profile extends Element {
                         }
                 }
                 this.sendGenres(arrayOfTrue);
+        }
+        seeGenres(){
+                this.genresOpen = true;
+        }
+        seePassword(){
+                this.genresOpen = false;
+        }
+        confirmPasswordChange(){
+                 var old = (<HTMLInputElement>document.getElementById("old-pass")).value;
+                 var newP = (<HTMLInputElement>document.getElementById("new-pass")).value;
+                 var confirm = (<HTMLInputElement>document.getElementById("confirm-new-pass")).value;
+                 this.changePass(old,newP,confirm);
         }
 }
